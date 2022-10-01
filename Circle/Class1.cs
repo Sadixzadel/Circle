28aee6fc7cc4db27444d3bfb8e7c6c8d44241cc4
HP <HP@DESKTOP-EQ9MNTC>
2022-10-02 01:29:00 +0400

    Добавьте файлы проекта.

---
+++ Circle/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2;
+
+class Circle
+{
+    // Your code here
+    public double Radius;
+    public Circle()
+    {
+
+    }
+
+    public Circle(double radius)
+    {
+        this.Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        double area = Math.PI * Radius * Radius;
+        return area;
+    }
+}
+
---
+++ Circle/Program.cs
@@ -0,0 +1,15 @@
+﻿using Homework2;
+
+Console.Write("Enter a radius for a circle: ");
+double radius = Double.Parse(Console.ReadLine()!);
+var circle = new Circle(radius);
+
+var area = circle.GetArea();
+Console.WriteLine($"Old area: {area}");
+
+var newRadius = 5.6;
+circle.Radius = newRadius;
+
+var newArea = circle.GetArea();
+Console.WriteLine($"New area: {newArea}");
+
